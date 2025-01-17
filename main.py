3d525798387ab0e422ab81d82d3df15ed9daa12d
Loading123747 <Utuber123wasd@gmail.com>
2025-01-16 19:32:04 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ PAGES = {
     "Play Basketball Stars": "Wing",
     "Play Slope": "Slither",
     "Play Granny": "geo",
+    "Play Bitlife": "life",
     "Play A Small World Cup": "cup",
     "Play Soccer Skills World Cup": "soccer2",
     "Play Soccer Skills Euro Cup": "soccer",
@@ -63,6 +64,8 @@ def main():
         play_level()
     elif st.session_state.page == "trial":
         play_trial()
+    elif st.session_state.page == "life":
+        play_life()
     elif st.session_state.page == "app":
         play_app()
     elif st.session_state.page == "basket":
@@ -100,19 +103,20 @@ def home():
     st.title("Welcome to CR7 Games! (Suuuuui!!!)")
     st.header("Shoutout to Skydagoat")
     st.subheader('Message from Skydagoat: "hop off')
+    st.subheader("use this link for another game site: https://loading123747.github.io/logan-edition/")
     st.subheader("if you want a shoutout tell me in live chat during 1st & 3rd period (you can also put a custom message)")
    # st.image(r, width=300)
     st.write("Before you start using the website read the notes in the information tab for information on how to use the website")
     st.write("Click the button below to open proxy!")
     #proxy greatsword
-    st.link_button("Proxy", "https://cr7games-first-heroku-site-16f8afd060a4.herokuapp.com")
-    st.link_button("Live Chat", "https://gydy8y-5000.csb.app/")
+    st.link_button("Proxy", "https://529wk5-8080.csb.app/")
+    st.link_button("Live Chat", "https://p6q25v-5000.csb.app/")
 
 def play_2048():
     st.title('Play 2048')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://betterproxy-71ec11ca7681.herokuapp.com/web/_aHR0cHM6Ly8yMDQ4Z2FtZS5jb20=_/'
+    game_url = 'https://2tgfnc-8080.csb.app/web/_aHR0cHM6Ly8yMDQ4Z2FtZS5jb20=_/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -130,7 +134,7 @@ def play_Slither():
     st.title('Play Slope')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://betterproxy-71ec11ca7681.herokuapp.com/web/_aHR0cHM6Ly9zbG9wZTMuY29t_/'
+    game_url = 'https://2tgfnc-8080.csb.app/web/_aHR0cHM6Ly9zbG9wZTMuY29t_/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -147,7 +151,7 @@ def play_powerline():
     st.title('Play 5 Nights At Freddys')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://betterproxy-71ec11ca7681.herokuapp.com/web/_aHR0cHM6Ly9maXZlbmlnaHRzYXRmcmVkZHlzMy5jb20=_/'
+    game_url = 'https://2tgfnc-8080.csb.app/web/_aHR0cHM6Ly9maXZlbmlnaHRzYXRmcmVkZHlzMy5jb20=_/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -167,7 +171,7 @@ def play_Wing():
     st.title('Play Basketball Stars')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://cr7games-first-heroku-site-16f8afd060a4.herokuapp.com/service/hvtrs8%2F-bcsievbclnsvaps%2Fgcmg.ko-/'
+    game_url = 'https://529wk5-8080.csb.app/service/hvtrs8%2F-bcsievbclnsvaps%2Fgcmg.ko-/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -205,10 +209,10 @@ def play_geo():
     #Greatsword
     st.title('Play Granny')
     st.write("Click the England flag for english then click play")
-    st.write("use this link while I try to fix the mouse locking problem: https://5vs4st-8080.csb.app/service/hvtrs8%2F-gpaln%7B-eaoeq.aoo%2F/ ")
+    st.write("use this link while I try to fix the mouse locking problem: https://529wk5-8080.csb.app/service/hvtrs8%2F-gpaln%7B-eaoeq.aoo%2F/ ")
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://cr7games-first-heroku-site-16f8afd060a4.herokuapp.com/hvtrs8%2F-gpaln%7B-eaoeq.aoo%2F/'
+    game_url = 'https://529wk5-8080.csb.app/service/hvtrs8%2F-gpaln%7B-eaoeq.aoo%2F/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -222,6 +226,24 @@ def play_geo():
     if st.button('Back to Home'):
         st.session_state.page = 'home'
 
+def play_life():
+    #Greatsword
+    st.title('Play Bitlife')
+
+    # URL of the game or webpage you want to embed
+    game_url = 'https://529wk5-8080.csb.app/service/hvtrs8%2F-bktnidemnnile%2Cgktju%60.ko-pna%7B.jtol/'
+
+    # HTML code to embed the game in an iframe with fullscreen option
+    iframe_code = f'''
+    <iframe src="{game_url}" width="100%" height="800px" style="border:none;" allowfullscreen></iframe>
+    '''
+
+    # Render the iframe in the Streamlit app
+    components.html(iframe_code, height=800)
+
+    # Button to go back to the home page
+    if st.button('Back to Home'):
+        st.session_state.page = 'home'
 
 def play_soccer():
     #Greatsword
@@ -229,7 +251,7 @@ def play_soccer():
     st.write("The display is small, sorry about that but if you can play like that go for it but I recommend fullscreen it")
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://cr7games-first-heroku-site-16f8afd060a4.herokuapp.com/hvtrs8%2F-tecmfdiaicl%2Cgktju%60.ko-gcmgs-smcaep-qkklns%2Fewrm-aur%2F/'
+    game_url = 'https://529wk5-8080.csb.app/service/hvtrs8%2F-tecmfdiaicl%2Cgktju%60.ko-gcmgs-smcaep-qkklns%2Fewrm-aur%2F/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -248,7 +270,7 @@ def play_soccer2():
     st.write("The display is small, sorry about that but if you can play like that go for it but I recommend fullscreen it")
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://cr7games-first-heroku-site-16f8afd060a4.herokuapp.com/hvtrs8%2F-tecmfdiaicl%2Cgktju%60.ko-gcmgs-smcaep-qkklns%2Fwmrnd%2Fcwp-/'
+    game_url = 'https://529wk5-8080.csb.app/service/hvtrs8%2F-tecmfdiaicl%2Cgktju%60.ko-gcmgs-smcaep-qkklns%2Fwmrnd%2Fcwp-/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -267,7 +289,7 @@ def play_cup():
     st.write("The display is small, sorry about that but if you can play like that go for it but I recommend fullscreen it")
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://cr7games-first-heroku-site-16f8afd060a4.herokuapp.com/hvtrs8%2F-tecmfdiaicl%2Cgktju%60.ko-gcmgs-a%2Fsoanl%2Fwmrnd%2Fcwp-/'
+    game_url = 'https://529wk5-8080.csb.app/service/hvtrs8%2F-tecmfdiaicl%2Cgktju%60.ko-gcmgs-a%2Fsoanl%2Fwmrnd%2Fcwp-/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -282,15 +304,16 @@ def play_cup():
         st.session_state.page = 'home'
 
 def play_level():
-    #Greatsword
+    # Greatsword
     st.title('Play Escape Road')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://cr7games-first-heroku-site-16f8afd060a4.herokuapp.com/hvtrs8%2F-gpaln%7B-eaoeq.aoo%2Fgsaare%2Frmaf/'
+    game_url = 'https://529wk5-8080.csb.app/service/hvtrs8%2F-gpaln%7B-eaoeq.aoo%2Fgsaare%2Frmaf/'
 
-    # HTML code to embed the game in an iframe with fullscreen option
+    # HTML code to embed the game in an iframe with pointer lock
     iframe_code = f'''
-    <iframe src="{game_url}" width="100%" height="800px" style="border:none;" allowfullscreen></iframe>
+    <iframe src="{game_url}" width="100%" height="800px" style="border:none;" 
+    allow="fullscreen; gamepad; pointer-lock"></iframe>
     '''
 
     # Render the iframe in the Streamlit app
@@ -300,14 +323,13 @@ def play_level():
     if st.button('Back to Home'):
         st.session_state.page = 'home'
 
-
 def play_basket():
     #Greatsword
     st.title('Play Galdihoppers')
     st.write("The display is small, sorry about that but if you can play like that go for it but I recommend fullscreen it")
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://cr7games-first-heroku-site-16f8afd060a4.herokuapp.com/hvtrs8%2F-tecmfdiaicl%2Cgktju%60.ko-gcmgs-gnafijorpgrq%2F/'
+    game_url = 'https://529wk5-8080.csb.app/service/hvtrs8%2F-tecmfdiaicl%2Cgktju%60.ko-gcmgs-gnafijorpgrq%2F/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -324,7 +346,7 @@ def mad():
     st.title('Play Drive Mad')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://betterproxy-71ec11ca7681.herokuapp.com/web/_aHR0cHM6Ly9kcml2ZW1hZC5tZQ==_/'
+    game_url = 'https://2tgfnc-8080.csb.app/web/_aHR0cHM6Ly9kcml2ZW1hZC5tZQ==_/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -344,25 +366,24 @@ def play_app():
     st.subheader("If it doesn't work, in the url (in the proxy below the actual url. it is in dark bule) enter the url (proxy url) shown on top of the buttons")
     st.title("            ")
     st.write("https://www.crazygames.com/")
-    st.link_button("Crazy Games", "https://stargersthings.blacknapkins.org/mycourses?login=aHR0cHM6Ly9jcmF6eWdhbWVzLmNvbQ%3D%3D")
+    st.link_button("Crazy Games", "https://use.heatmypool.com/mycourses?login=aHR0cHM6Ly9jcmF6eWdhbWVzLmNvbQ%3D%3D")
     st.write("https://now.gg/")
-    st.link_button("Now.gg", "https://stargersthings.blacknapkins.org/mycourses?login=aHR0cHM6Ly9lZHVjYXRpb25ibHVlc2t5LmNvbS9hcHBzL3JvYmxveC1jb3Jwb3JhdGlvbi81MzQ5L3JvYmxveC5odG1s")
+    st.link_button("Now.gg", "https://use.heatmypool.com/mycourses?login=aHR0cHM6Ly9lZHVjYXRpb25ibHVlc2t5LmNvbS9hcHBzL3JvYmxveC1jb3Jwb3JhdGlvbi81MzQ5L3JvYmxveC5odG1s")
     st.write("https://www.chess.com/")
-    st.link_button("Chess.com", "https://stargersthings.blacknapkins.org/mycourses?login=aHR0cHM6Ly9jaGVzcy5jb20%3D")
+    st.link_button("Chess.com", "https://use.heatmypool.com/mycourses?login=aHR0cHM6Ly9jaGVzcy5jb20%3D")
     st.write("https://www.tiktok.com/explore")
-    st.link_button("Tiktok", "https://stargersthings.blacknapkins.org/mycourses?login=aHR0cHM6Ly90aWt0b2suY29t")
+    st.link_button("Tiktok", "https://use.heatmypool.com/mycourses?login=aHR0cHM6Ly90aWt0b2suY29t")
     st.write("https://www.coolmathgames.com/")
-    st.link_button("CoolMath Games", "https://stargersthings.blacknapkins.org/mycourses?login=aHR0cHM6Ly9jb29sbWF0aGdhbWVzLmNvbQ%3D%3D")
+    st.link_button("CoolMath Games", "https://use.heatmypool.com/mycourses?login=aHR0cHM6Ly9jb29sbWF0aGdhbWVzLmNvbQ%3D%3D")
     st.write("https://9amime.com/")
-    st.link_button("9amine", "https://stargersthings.blacknapkins.org/mycourses?login=aHR0cHM6Ly85YW5pbWV0di50bw%3D%3D")
+    st.link_button("9amine", "https://use.heatmypool.com/mycourses?login=aHR0cHM6Ly85YW5pbWV0di50bw%3D%3D")
     st.write("https://SFlix.com/")
-    st.link_button("Twitch", "https://stargersthings.blacknapkins.org/mycourses?login=aHR0cHM6Ly90d2l0Y2gudHY%3D")
+    st.link_button("Twitch", "https://use.heatmypool.com/mycourses?login=aHR0cHM6Ly90d2l0Y2gudHY%3D")
     st.write("https://Netflix.com/")
-    st.link_button('Netflix', "https://stargersthings.blacknapkins.org/mycourses?login=aHR0cHM6Ly9uZXRmbGl4LmNvbQ%3D%3D")
+    st.link_button('Netflix', "https://use.heatmypool.com/mycourses?login=aHR0cHM6Ly9uZXRmbGl4LmNvbQ%3D%3D")
     st.write("https://bobzgames.github.io/GBA/")
-    st.link_button("GBA Emulator", "https://stargersthings.blacknapkins.org/mastery?auth=5sr02nn")
-    st.link_button("Extra Games", "https://stargersthings.blacknapkins.org/gms")
-    st.link_button("Better Proxy", "https://stargersthings.blacknapkins.org/")
+    st.link_button("GBA Emulator", "https://use.heatmypool.com/mastery?auth=5sr02nn")
+    st.link_button("Extra Games", "https://use.heatmypool.com/gms")
     if st.button('Back to Home'):
         st.session_state.page = 'home'
 
@@ -370,7 +391,7 @@ def play_trial():
     st.title('Play Level Devil')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://betterproxy-71ec11ca7681.herokuapp.com/web/_aHR0cHM6Ly93d3cuc25va2lkby5jb20=_/game/level-devil'
+    game_url = 'https://2tgfnc-8080.csb.app/web/_aHR0cHM6Ly93d3cuc25va2lkby5jb20=_/game/level-devil'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -388,7 +409,7 @@ def play_cross():
     st.title('Play Crossy Road')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://betterproxy-71ec11ca7681.herokuapp.com/web/_aHR0cHM6Ly93d3cuc25va2lkby5jb20=_/game/crossy-road'
+    game_url = 'https://2tgfnc-8080.csb.app/web/_aHR0cHM6Ly93d3cuc25va2lkby5jb20=_/game/crossy-road'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -406,7 +427,7 @@ def play_retro():
     st.title('Play Retro Bowl')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://betterproxy-71ec11ca7681.herokuapp.com/web/_aHR0cHM6Ly9yZXRyb2Jvd2wubWU=_/'
+    game_url = 'https://2tgfnc-8080.csb.app/web/_aHR0cHM6Ly9yZXRyb2Jvd2wubWU=_/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -424,7 +445,7 @@ def play_get():
     st.title('Play Getaway Shootout')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://betterproxy-71ec11ca7681.herokuapp.com/web/_aHR0cHM6Ly90Z2NvZmZpY2lhbC5naXRodWIuaW8=_/games/getaway-shootout/'
+    game_url = 'https://2tgfnc-8080.csb.app/web/_aHR0cHM6Ly90Z2NvZmZpY2lhbC5naXRodWIuaW8=_/games/getaway-shootout/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -443,9 +464,9 @@ def play_lol():
     st.title('Play 1v1 LOL')
 
     # URL of the game or webpage you want to embed
-    st.write("somtimes it is a bit glitchy and if it is go to this link: https://betterproxy-71ec11ca7681.herokuapp.com/web/_aHR0cHM6Ly8xdjFsb2wubWU=_/")
+    st.write("somtimes it is a bit glitchy and if it is go to this link: https://2tgfnc-8080.csb.app/web/_aHR0cHM6Ly8xdjFsb2wubWU=_/")
     st.write("to play with friends or 1v1 other real people click on the plus icon and then click create party. share the code to play with friends (only works with 2 people including you, or don't share to play by your self or online player) then click play only party leader can click play")
-    game_url = 'https://betterproxy-71ec11ca7681.herokuapp.com/web/_aHR0cHM6Ly8xdjFsb2wubWU=_/'
+    game_url = 'https://2tgfnc-8080.csb.app/web/_aHR0cHM6Ly8xdjFsb2wubWU=_/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -463,7 +484,7 @@ def play_uno():
     st.title('Play Uno')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://betterproxy-71ec11ca7681.herokuapp.com/web/_aHR0cHM6Ly9nZXRhd2F5LXNob290b3V0LmNvbQ==_/uno-online'
+    game_url = 'https://2tgfnc-8080.csb.app/web/_aHR0cHM6Ly9nZXRhd2F5LXNob290b3V0LmNvbQ==_/uno-online'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -483,7 +504,7 @@ def play_ovo():
     #use v4 proxy for this
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://stargersthings.blacknapkins.org/mycourses?login=aHR0cHM6Ly91bmJsb2NrZWQtZ2FtZXMuczMuYW1hem9uYXdzLmNvbS9vdm8uaHRtbA%3D%3D'
+    game_url = 'https://use.heatmypool.com/mycourses?login=aHR0cHM6Ly91bmJsb2NrZWQtZ2FtZXMuczMuYW1hem9uYXdzLmNvbS9vdm8uaHRtbA%3D%3D'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
@@ -502,7 +523,7 @@ def play_cluster():
     st.title('Play Cluster Rush')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://cr7games-first-heroku-site-16f8afd060a4.herokuapp.com/hvtrs8%2F-cnuqtgrpuqh%2Cim%2F/'
+    game_url = 'https://529wk5-8080.csb.app/service/hvtrs8%2F-cnuqtgrpuqh%2Cim%2F/'
 
     # HTML code to embed the game in an iframe with fullscreen option
     iframe_code = f'''
